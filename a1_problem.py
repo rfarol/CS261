f9757397a4f915113a30b8d1a41b46044b1e95cb
rfarol <farolr@oregonstate.edu>
2020-10-14 18:22:13 -0700

    Min and Max

--- a1_problem.py
+++ a1_problem.py
@@ -1,6 +1,6 @@
 # Course: CS261 - Data Structures
-# Student Name:
-# Assignment:
+# Student Name: Ryan Farol
+# Assignment: Assignment 1 Problem 1
 # Description:
 
 
@@ -8,10 +8,18 @@ from a1_include import *
 
 
 def min_max(arr: StaticArray) -> ():
-    """
-    TODO: Write this implementation
-    """
-    return 0, 0
+    """function takes the first integer on the static array and iterates through the entire list while comparing the
+    previous value to the current position. If it's higher, it will take the place as the max integer. If it is lower,
+    it will take the place as the min integer."""
+    list_size = arr.size() # takes size of the array
+    max_int = arr.get(index=0) # initiliaze index = 0 as the max
+    min_int = max_int # set the max as min
+    for i in range(list_size):
+        if arr[i] > max_int:  # checks if the remaining integers is greater than first integer entered
+            max_int = arr[i]
+        if arr[i] < min_int:  # checks if the remaining integers is less than first integer entered
+            min_int = arr[i]
+    return min_int, max_int
 
 
 # BASIC TESTING
