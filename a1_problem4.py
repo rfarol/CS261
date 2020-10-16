c032ca9b50bf26842c0c1f36414f64fb5b338940
rfarol <farolr@oregonstate.edu>
2020-10-15 23:19:41 -0700

    Min and Max

--- a1_problem4.py
+++ a1_problem4.py
@@ -8,10 +8,32 @@ from a1_include import *
 
 
 def rotate(arr: StaticArray, steps: int) -> StaticArray:
-    """
-    TODO: Write this implementation
-    """
-    return StaticArray()
+    empty = [_ for _ in range(arr.size())] # create empty static array and use size of current array as length
+    fb_arr = StaticArray(len(empty)) # call static array class to make new array
+    for i, value in enumerate(empty): # enumerate list and give it values
+        fb_arr[i] = value
+    first_count = 1
+    prev = 0
+    counter = steps
+    while counter != 0:
+        if counter == 0:
+            break
+        else:
+            for i in range(arr.size()):  # iterate through current array and copy the contents over
+                if steps > 0:
+                    fb_arr.set(first_count, arr.get(index=prev))
+                    first_count = first_count + 1
+                    prev = prev + 1
+                    counter = counter - 1
+                    if first_count == fb_arr.size():
+                        first_count = 0
+                    if prev == fb_arr.size():
+                        prev = 0
+            return fb_arr
+
+
+
+
 
 
 
--- a1_problem5.py
+++ a1_problem5.py
@@ -1,17 +1,37 @@
 # Course: CS261 - Data Structures
-# Student Name:
-# Assignment:
-# Description:
+# Student Name: Ryan Farol
+# Assignment: Assignment 1 Problem 5
+# Description: Takes a range of numbers and outputs an array of that ranage
 
 
 from a1_include import *
 
 
 def sa_range(start: int, end: int) -> StaticArray:
-    """
-    TODO: Write this implementation
-    """
-    return StaticArray()
+    if end < start and end < 0: # checks if the end range is smaller than the start range for negative values
+        empty = [_ for _ in range(abs(start), abs(end)+1)]  # create empty static array and use range to determine the size
+        fb_arr = StaticArray(len(empty))  # call static array class to make new array
+        for i, value in enumerate(empty):  # enumerate list and give it values
+            fb_arr[i] = -value # adds a negative value to the array
+        return fb_arr
+
+    elif end > start and end < 0: # checks if the end range is larger than the start range
+        empty = [_ for _ in range(abs(start), abs(end)-1, -1)]  # create empty static array and use range to determine length. Reverses the numbers
+        fb_arr = StaticArray(len(empty))  # call static array class to make new array
+        for i, value in enumerate(empty):  # enumerate list and give it values
+            fb_arr[i] = -value # adds a negative value to the array
+        return fb_arr
+
+    else:
+        empty = [_ for _ in range(start, abs(end)+1)]  # create empty static array and range for the size of the array
+        fb_arr = StaticArray(len(empty))  # call static array class to make new array
+        if end < 0: # checks if end is smaller than 0 for negative numbers
+            for i, value in enumerate(empty):  # enumerate list and give it values
+                fb_arr[i] = -value
+        else:
+            for i, value in enumerate(empty):  # enumerate list and give it values
+                fb_arr[i] = value
+        return fb_arr
 
 
 # BASIC TESTING
