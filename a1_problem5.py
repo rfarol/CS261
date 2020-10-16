b9d88d3b39d4475ca9f380d82e9e1d3c007e55d3
rfarol <farolr@oregonstate.edu>
2020-10-15 23:34:55 -0700

    Min and Max

--- a1_problem5.py
+++ a1_problem5.py
@@ -29,6 +29,13 @@ def sa_range(start: int, end: int) -> StaticArray:
             fb_arr[i] = value  # adds a negative value to the array
         return fb_arr
 
+    elif end == start and end and start < 0:
+        empty = [_ for _ in range(abs(start), abs(end) + 1)]  # create empty static array and range for the size of the array
+        fb_arr = StaticArray(len(empty))  # call static array class to make new array
+        for i, value in enumerate(empty):  # enumerate list and give it values
+            fb_arr[i] = -value
+        return fb_arr
+
     else:
         empty = [_ for _ in range(start, abs(end)+1)]  # create empty static array and range for the size of the array
         fb_arr = StaticArray(len(empty))  # call static array class to make new array
@@ -47,6 +54,6 @@ if __name__ == "__main__":
     # example 1
     cases = [
         (1, 3), (-1, 2), (0, 0), (0, -3),
-        (-105, -99), (-99, -105)]
+        (-105, -99), (-99, -105), (-1,-1)]
     for start, end in cases:
         print(start, end, sa_range(start, end))
